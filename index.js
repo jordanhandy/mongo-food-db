a4fb19c080d6ca674a7b7a690fc6315c1d0f0cba
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2021-07-17 19:03:54 -0400

    add path for finding all products

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ mongoose.connect('mongodb://localhost:27017/farmStand',{useNewUrlParser: true, u
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
-app.get('/dog',(req,res)=>{
-    res.send('woof!');
+app.get('/products', async (req,res)=>{
+    const products = await Product.find({});
+    console.log(products);
+    res.send('ALL PRODUCTS WILL BE HERE');
 })
 app.listen(3000,()=>{
     console.log('listening on port 3000');
