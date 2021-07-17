8ce5ac4a864a0025a5d3b64b63f81d9b5c66adcd
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2021-07-17 18:21:54 -0400

    init commit, express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const { appendFileSync } = require('fs');
+
+mongoose.connect('mongodb://localhost:27017/camping',{useNewUrlParser: true}).then(() =>{
+    console.log("mongo connection open!");
+}).catch((err)=>{
+    console.log('Could not connect to mongo');
+});
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.get('/dog',(req,res)=>{
+    res.send('woof!');
+})
+app.listen(3000,()=>{
+    console.log('listening on port 3000');
+})
