61dc3027115c0e229471f51f53bdfa9443c1dc18
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2021-07-17 20:26:01 -0400

    fix logic for submitting post form

--- index.js
+++ index.js
@@ -13,24 +13,36 @@ mongoose.connect('mongodb://localhost:27017/farmStand',{useNewUrlParser: true, u
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}))
 
 app.get('/products', async (req,res)=>{
     const products = await Product.find({});
-    console.log(products);
     res.render('products/index',{
         products:products
     });
+})
+app.get('/products/new',(req,res)=>{
+    res.render('products/new');
 })
 app.get('/products/:id',async(req,res)=>{
     const product = await Product.findById(req.params.id); // find by id, given id params
     console.log(product);
     res.render('products/details',{
         product:product
-    })
+    });
 })
 
-app.get('/products/new',(req,res)=>{
-    res.render('products/new');
+
+app.post('/products',async (req,res)=>{
+    product = new Product({
+        name: req.body.name,
+        price:req.body.price,
+        category:req.body.category
+    });
+    await product.save().then((res)=>{
+        console.log('saved');
+    })
+    res.redirect('/products');
 })
 
 app.listen(3000,()=>{
