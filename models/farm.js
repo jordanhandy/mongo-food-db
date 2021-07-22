4cb2b19527f2406ae4c3c578444201c32f0468a7
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2021-07-21 21:06:30 -0400

    complete farm schema

--- models/farm.js
+++ models/farm.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose');
 const farmSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required: [true, 'Farm must have a name']
+    },
+    city: {
+        type:String,
+    },
+    email:{
+        type:String,
+        required:[true, 'Email is required']
+    }
 
 })
 const Farm = mongoose.model("Farm",farmSchema);
