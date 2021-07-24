574f7436ad3992518f077f46091c685945a237b6
Jordan Handy <6423379+jordanhandy@users.noreply.github.com>
2021-07-24 07:36:08 -0400

    slightly edit middleware for logging

--- models/farm.js
+++ models/farm.js
@@ -25,7 +25,8 @@ const farmSchema = new mongoose.Schema({
 farmSchema.post('findOneAndDelete',async function(farm){
     if(farm.products.length){
         // Delete all products whose ID is in the products array
-        Product.deleteMany({_id:{$in: farm.products}});
+        const data = await Product.deleteMany({_id:{$in: farm.products}});
+        console.log("deleted all products from farm",data);
     }
 })
 const Farm = mongoose.model("Farm",farmSchema);
